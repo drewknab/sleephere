4d9f306d3f3e780f3a5aab99971989eadf47b538
Drew Knab <aknab@sidearmsports.com>
2020-02-28 08:22:43 -0500

    more messages

--- main.js
+++ main.js
@@ -2,16 +2,32 @@
   let $main = document.querySelector("main");
   const theDate = new Date();
 
+  const goHomeMessages = [
+    "What are you sleeping here?",
+    "Did you bring a tent?",
+    "You gonna take down the cot?",
+    "Aren't you going to put away the sleeping bag?",
+    "Slack === slacking",
+  ];
+
+  const focusMessages = [
+    "Salad is not a soup, please focus on work.",
+    "Please focus on actually working and not slacking.",
+    "slack <img src='/images/snowflake.png'>"
+  ];
+
+  const getMessages = (messageArray) => messageArray[Math.floor(Math.random() * messageArray.length)];
+
   if (theDate.getHours() >= 17)
   {
     $main.classList.add("warning");
     $main.innerHTML = /*html*/`
-      <p>What are you sleeping here? Go home.</p>
+      <p>${getMessages(goHomeMessages)}</p>
     `;
   }
   else {
     $main.innerHTML = /*html*/`
-      <p>Keep Working.</p>
+      <p><img src='/images/lyindrei.png'>${getMessages(focusMessages)}</p>
     `;
   }
 })();
